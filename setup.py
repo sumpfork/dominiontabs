d7a1337d9c4ef9ff75758663ddac8f9ae8b23b05
sumpfork@mailmight.com <sumpfork@mailmight.com>
2010-10-28 14:47:10 -0700

    Setup scripts, readme, distribute bootstrap

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup,find_packages
+
+setup(
+    name="dominiontabs",
+    version="1.1",
+    packages=find_packages(),
+    scripts=["dominion_tabs.py"],
+    install_requires=["reportlab>=2.5",
+                      "PIL>=1.1.7"],
+    package_data = {
+        '' : ['*.txt','*.png']
+        },
+    author="Sumpfork",
+    author_email="sumpfork@mailmight.net",
+    description="Tab Divider Generation for the Dominion Card Game"
+    )
