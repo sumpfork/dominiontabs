e377a06876c67d57baf2ac1916185dd0ad3475ce
Sumpfork <github@petergorniak.org>
2015-06-05 10:20:38 -0700

    add yaml to setup, replace PIL with Pillow, bump version

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-#main package
+# main package
 
-__version__ = '1.9'
+__version__ = '1.9.1'
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from __init__ import __version__
 from distribute_setup import use_setuptools
+from setuptools import setup, find_packages
+
 use_setuptools()
 
-from setuptools import setup,find_packages
 
 setup(
     name="dominiontabs",
@@ -10,12 +11,13 @@ setup(
     scripts=["dominion_tabs.py"],
     packages=find_packages(),
     install_requires=["reportlab>=2.5",
-                      "PIL>=1.1.7"],
-    package_data = {
-        '' : ['*.txt','*.png']
-        },
+                      "Pillow>=2.1.0",
+                      "PyYAML"],
+    package_data={
+        '': ['*.txt', '*.png']
+    },
     author="Sumpfork",
     author_email="sumpfork@mailmight.net",
     description="Tab Divider Generation for the Dominion Card Game"
-    )
+)
 
