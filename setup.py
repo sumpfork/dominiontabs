c4c44bfbea8ef27d79fc9ef6f27e208e0c3faca0
Sumpfork <sumpfork@mailmight.com>
2017-01-08 11:03:28 -0800

    Increase minor version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="domdiv",
-    version='3.0.0',
+    version='3.1.0',
     entry_points={
         'console_scripts': [
             "dominion_dividers = domdiv.main:main"
