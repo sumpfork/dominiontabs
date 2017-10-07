41ca63020dc2d6e22e890f1bf29e0de2956b74cc
Sumpfork <sumpfork@mailmight.com>
2017-10-07 12:20:40 -0700

    put in correct version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="domdiv",
-    version='3.2.1',
+    version='3.3.0',
     entry_points={
         'console_scripts': [
             "dominion_dividers = domdiv.main:main"
