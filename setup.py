10b8e858cb525c02e481484a89e3239800035ff1
Sumpfork <sumpfork@mailmight.com>
2017-11-17 16:22:28 -0800

    minor version bump for sorting issue; accept new reportlab version again, they seem to work once more

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="domdiv",
-    version='3.3.0',
+    version='3.3.1',
     entry_points={
         'console_scripts': [
             "dominion_dividers = domdiv.main:main"
         ],
     },
     packages=find_packages(exclude=['tests']),
-    install_requires=["reportlab==3.3.0",
+    install_requires=["reportlab>=3.4.0",
                       "Pillow>=4.1.0"],
     include_package_data=True,
     author="Sumpfork",
