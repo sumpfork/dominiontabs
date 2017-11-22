883fb8eac703c6b1b7ddf2acfed544c7f41909ea
Sébastien Wautelet <sebwautelet@gmail.com>
2017-11-22 10:32:04 +0100

    update from upstream (#1)
    * Complete French translation for base set (1st edition) (#170)
    
    * French translation for Dominion set (1st edition)
    
    * work on distribution
    
    * update readme

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
+version = '3.3.2'
+
 setup(
     name="domdiv",
-    version='3.3.1',
+    version=version,
     entry_points={
         'console_scripts': [
             "dominion_dividers = domdiv.main:main"
@@ -11,8 +13,12 @@ setup(
     packages=find_packages(exclude=['tests']),
     install_requires=["reportlab>=3.4.0",
                       "Pillow>=4.1.0"],
+    url='http://domtabs.sandflea.org',
     include_package_data=True,
-    author="Sumpfork",
+    author="Peter Gorniak",
     author_email="sumpfork@mailmight.net",
-    description="Divider Generation for the Dominion Card Game"
+    description="Divider Generation for the Dominion Card Game",
+    keywords=['boardgame', 'cardgame', 'dividers'],
+    long_description="This script and library generate dividers for the Dominion Card Game by Rio Grande Games.\
+     See it in action at http://domtabs.sandflea.org."
 )
