eff6d2796b65848eefeeaa861800c0d0e991c363
Sumpfork <sumpfork@mailmight.com>
2017-11-29 11:20:38 -0800

    Quick fix to release script.

--- do_release.py
+++ do_release.py
@@ -1,17 +1,20 @@
 import domdiv
-from __init__ import __version__
+import domdiv.main
 from zipfile import ZipFile, ZIP_DEFLATED
 
 prefix = 'generated/sumpfork_dominion_tabs_'
-postfix = 'v' + __version__ + '.pdf'
+postfix = 'v' + domdiv.__version__ + '.pdf'
 
 
 def doit(args, main):
-    args = args + ' ' + prefix + main + postfix
+    args = args + ' --outfile ' + prefix + main + postfix
     args = args.split()
     fname = args[-1]
+    print args
     print ':::Generating ' + fname
-    domdiv.main(args, '.')
+    options = domdiv.main.parse_opts(args)
+    options = domdiv.main.clean_opts(options)
+    domdiv.main.generate(options)
     return fname
 
 
@@ -28,7 +31,7 @@ fnames = [doit(args[0] + ' ' + ' '.join(additional), args[1])
           for args in argsets]
 print fnames
 
-zip = ZipFile('generated/sumpfork_dominion_tabs_v' + __version__ + '.zip', 'w',
+zip = ZipFile('generated/sumpfork_dominion_tabs_v' + domdiv.__version__ + '.zip', 'w',
               ZIP_DEFLATED)
 for f in fnames:
     zip.write(f)
