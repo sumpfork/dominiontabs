01b0cddd154c0ee3d6088e1a8410bc936cdef1c1
Sumpfork <sumpfork@mailmight.com>
2018-01-15 22:10:08 -0800

    switch to pytest; start adding language tool test

--- domdiv/tests/carddb_tests.py
+++ domdiv/tests/carddb_tests.py
@@ -1,58 +1,85 @@
-import unittest
+from __future__ import print_function
+import subprocess
+import shutil
+import os
+
+import pytest
+
 from .. import main
 from .. import cards as domdiv_cards
 
 
-class TestCardDB(unittest.TestCase):
+@pytest.fixture
+def rmtestcardb(request):
 
-    def test_cardread(self):
-        options = main.parse_opts([])
+    def rmd():
+        testcardb_dir = os.path.join(str(request.config.rootdir), 'tools/card_db')
+        if os.path.exists(testcardb_dir):
+            print('removing {}'.format(testcardb_dir))
+            shutil.rmtree(testcardb_dir)
+    request.addfinalizer(rmd)
+
+
+def test_cardread():
+    options = main.parse_opts([])
+    options.data_path = '.'
+    cards = main.read_card_data(options)
+    assert len(cards) == 524
+    valid_cardsets = {
+        u'base',
+        u'dominion1stEdition',
+        u'dominion2ndEdition',
+        u'dominion2ndEditionUpgrade',
+        u'intrigue1stEdition',
+        u'intrigue2ndEdition',
+        u'intrigue2ndEditionUpgrade',
+        u'seaside',
+        u'alchemy',
+        u'prosperity',
+        u'cornucopia extras',
+        u'cornucopia',
+        u'hinterlands',
+        u'dark ages',
+        u'dark ages extras',
+        u'guilds',
+        u'adventures',
+        u'adventures extras',
+        u'empires',
+        u'empires extras',
+        u'nocturne',
+        u'nocturne extras',
+        u'promo',
+        u'extras',
+        u'animals'
+    }
+    for c in cards:
+        assert isinstance(c, domdiv_cards.Card)
+        assert c.cardset_tag in valid_cardsets
+
+
+def test_languages():
+    languages = main.get_languages('card_db')
+    for lang in languages:
+        print('checking ' + lang)
+        # for now, just test that they load
+        options = main.parse_opts(['--language', lang])
         options.data_path = '.'
         cards = main.read_card_data(options)
-        self.assertEquals(len(cards), 524)
-        valid_cardsets = {
-            u'base',
-            u'dominion1stEdition',
-            u'dominion2ndEdition',
-            u'dominion2ndEditionUpgrade',
-            u'intrigue1stEdition',
-            u'intrigue2ndEdition',
-            u'intrigue2ndEditionUpgrade',
-            u'seaside',
-            u'alchemy',
-            u'prosperity',
-            u'cornucopia extras',
-            u'cornucopia',
-            u'hinterlands',
-            u'dark ages',
-            u'dark ages extras',
-            u'guilds',
-            u'adventures',
-            u'adventures extras',
-            u'empires',
-            u'empires extras',
-            u'nocturne',
-            u'nocturne extras',
-            u'promo',
-            u'extras',
-            u'animals'
-        }
-        for c in cards:
-            self.assertIsInstance(c, domdiv_cards.Card)
-            self.assertIn(c.cardset_tag, valid_cardsets)
+        assert cards, '"{}" cards did not read properly'.format(lang)
+        cards = main.add_card_text(options, cards, 'en_us')
+        cards = main.add_card_text(options, cards, lang)
+        if lang == 'it':
+            assert "Maledizione" in [card.name for card in cards]
+        elif lang == 'de':
+            assert "Fluch" in [card.name for card in cards]
 
-    def test_languages(self):
-        languages = main.get_languages('card_db')
-        for lang in languages:
-            print('checking ' + lang)
-            # for now, just test that they load
-            options = main.parse_opts(['--language', lang])
-            options.data_path = '.'
-            cards = main.read_card_data(options)
-            self.assertTrue(cards, '"{}" cards did not read properly'.format(lang))
-            cards = main.add_card_text(options, cards, 'en_us')
-            cards = main.add_card_text(options, cards, lang)
-            if lang == 'it':
-                self.assertIn("Maledizione", [card.name for card in cards])
-            elif lang == 'de':
-                self.assertIn("Fluch", [card.name for card in cards])
+
+def test_languagetool_run(pytestconfig):
+    cmd = 'cd {} ; python tools/update_language.py'.format(pytestconfig.rootdir)
+    print(cmd)
+    assert subprocess.check_call(cmd.split()) == 0
+    cmd = 'cd {} ; diff -r domdiv/card_db tols/card_db'.format(pytestconfig.rootdir)
+    print(subprocess.check_output(['ls', os.path.join(str(pytestconfig.rootdir), 'tools/card_db')]))
+    out = subprocess.check_output(cmd.split(), stderr=subprocess.STDOUT)
+    print(out)
+    assert out == ''
