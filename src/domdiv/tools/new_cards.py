0bd81f8cf21feaf6239ba7b7f59f2bb7a40558be
Peter Gorniak <sumpfork@mailmight.com>
2020-03-23 21:43:33 -0700

    run black

--- src/domdiv/tools/new_cards.py
+++ src/domdiv/tools/new_cards.py
@@ -5,10 +5,11 @@ import csv
 import json
 import argparse
 
+
 def parse_opts(cmdline_args=None):
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description='''From a CSV file containing card information, generate JSON code that can be added
+        description="""From a CSV file containing card information, generate JSON code that can be added
                        to the cards_db.json and /en_us/cards_en_us.json files.
                        Column headings used include:
                        "Sets";"Card";"Cost";"Count";"Randomizer";"Types";"Group";"Description";"Extra".
@@ -23,10 +24,13 @@ def parse_opts(cmdline_args=None):
                        Cards with the same "Group" value will be group together with --special-card-groups.
                        A row should then be added with a blank "Sets" with this group value as a "Card" to include
                        the text values for the tab used by the group.
-        ''',
+        """,
         epilog="Source can be found at 'https://github.com/sumpfork/dominiontabs'. ",
     )
-    parser.add_argument('csv', help='input CSV file saved in UTF-8 format with ";" as the column seperator.')
+    parser.add_argument(
+        "csv",
+        help='input CSV file saved in UTF-8 format with ";" as the column seperator.',
+    )
 
     # Basic Divider Information
     group_basic = parser.add_argument_group(
@@ -60,18 +64,14 @@ def generate(options):
     new_cards_en_us = {}
 
     with open(options.csv) as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=';', quotechar='"', escapechar='')
+        reader = csv.DictReader(csvfile, delimiter=";", quotechar='"', escapechar="")
         # NOTE: csv file must be saved in UTF-8
         for card in reader:
             card_db_entry = {
                 "card_tag": card["Card"].strip(),
-                "cardset_tags": [
-                    s.strip() for s in card["Sets"].split(",")
-                ],
+                "cardset_tags": [s.strip() for s in card["Sets"].split(",")],
                 "cost": card["Cost"].strip(),
-                "types": [
-                    t.strip() for t in card["Types"].split(",")
-                ],
+                "types": [t.strip() for t in card["Types"].split(",")],
             }
 
             if card["Randomizer"]:
@@ -93,10 +93,10 @@ def generate(options):
                 name = card["Group"].strip()
 
             new_cards_en_us[card["Card"]] = {
-                    "description" : card["Description"].replace('\\"', '"').strip(),
-                    "extra" : card["Extra"].replace('\\"', '"').strip(),
-                    "name": name
-                }
+                "description": card["Description"].replace('\\"', '"').strip(),
+                "extra": card["Extra"].replace('\\"', '"').strip(),
+                "name": name,
+            }
 
     json.dump(new_cards_db, open(options.cards_db, "w"), indent=4)
     json.dump(new_cards_en_us, open(options.cards_text, "w"), indent=4)
@@ -107,5 +107,6 @@ def main():
     options = clean_opts(options)
     generate(options)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
